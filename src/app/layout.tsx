d864ad7f380332b8e97914d064f41474e63e33dc
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-10-21 00:28:55 +0530

    Update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,30 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Amigo Brain",
-  description: "Do You Know Me?",
+  title: "AmigoBrain - Create and Play Fun Quiz with Friends",
+  description:
+    "Create personalized quiz about yourself, share with friends, and see who knows you best! Play, score, and compare with the Friendboard.",
+  openGraph: {
+    title: "AmigoBrain - Create and Play Fun Quiz",
+    description:
+      "Create personalized quiz about yourself, share with friends, and see who knows you best! Play, score, and check the Friendboard for results.",
+    url: "https://amigo-brain.vercel.app/",
+    images: [
+      {
+        url: "https://amigo-brain.vercel.app/amigo-brain/image_2.png",
+        alt: "AmigoBrain Quiz App",
+      },
+    ],
+    siteName: "AmigoBrain",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AmigoBrain - Fun Quiz with Friends",
+    description:
+      "Create quiz, share them with friends, and see who knows you best! Check the Friendboard for the final score.",
+    images: "https://amigo-brain.vercel.app/amigo-brain/image_2.png",
+  },
 };
 
 export default function RootLayout({
